83e5eac6c742f38b988932a06cb3cfe8c6d7c945
Blair Lyons <blair208@gmail.com>
2020-09-21 14:13:16 -0700

    updated physicell to simularium conversion to use new simulariumio pkg

--- analysis/conversion/physicell_to_simularium.py
+++ analysis/conversion/physicell_to_simularium.py
@@ -1,111 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import json
-from pathlib import Path
+import argparse
 
 import numpy as np
-from .pyMCDS import pyMCDS
+from simulariumio import Converter
 
-
-def load_simulation_data(path_to_xml_files):
-    """ load simulation data from PhysiCell MultiCellDS XML files """
-    sorted_files = sorted(Path(path_to_xml_files).glob("output*.xml"))
-    data = []
-    for file in sorted_files:
-        data.append(pyMCDS(file.name, False, path_to_xml_files))
-    return np.array(data)
-
-
-ids = {}
-last_id = 0
-
-
-def get_agent_type(cell_type, cell_phase):
-    """ get a unique agent type id for a specific cell type and phase combination """
-    global ids, last_id
-
-    if cell_type not in ids:
-        ids[cell_type] = {}
-
-    if cell_phase not in ids[cell_type]:
-        print(
-            "ID {} : cell type = {}, cell phase = {}".format(
-                last_id, cell_type, cell_phase
-            )
-        )
-        ids[cell_type][cell_phase] = last_id
-        last_id += 1
-
-    return ids[cell_type][cell_phase]
-
-
-def get_visualization_data_one_frame(index, sim_data_one_frame):
-    """ get data from one time step in Simularium format """
-    discrete_cells = sim_data_one_frame.get_cell_df()
-
-    data = {}
-    data["data"] = []
-    data["frameNumber"] = index
-    data["time"] = index
-
-    for i in range(len(discrete_cells["position_x"])):
-
-        data["data"].append(1000.0)  # viz type = sphere
-        data["data"].append(
-            float(
-                get_agent_type(
-                    int(discrete_cells["cell_type"][i]),  # agent type
-                    int(discrete_cells["current_phase"][i]),
-                )
-            )
-        )
-        data["data"].append(round(discrete_cells["position_x"][i], 1))  # X position
-        data["data"].append(round(discrete_cells["position_y"][i], 1))  # Y position
-        data["data"].append(round(discrete_cells["position_z"][i], 1))  # Z position
-        data["data"].append(
-            round(discrete_cells["orientation_x"][i], 1)
-        )  # X orientation
-        data["data"].append(
-            round(discrete_cells["orientation_x"][i], 1)
-        )  # Y orientation
-        data["data"].append(
-            round(discrete_cells["orientation_x"][i], 1)
-        )  # Z orientation
-        data["data"].append(
-            round(np.cbrt(3.0 / 4.0 * discrete_cells["total_volume"][i] / np.pi), 1)
-        )  # scale
-        data["data"].append(0.0)  # ?
-    return data
-
-
-def convert_physicell_to_simularium(sim_data):
-    """ get data in Simularium format """
-    data = {}
-    data["msgType"] = 1
-    data["bundleStart"] = 0
-    data["bundleSize"] = len(sim_data)
-    data["bundleData"] = []
-    for i in range(len(sim_data)):
-        data["bundleData"].append(get_visualization_data_one_frame(i, sim_data[i]))
-    return data
-
-
-def write_json_data(viz_data, json_output_path="output", model_name="new_model"):
-    """ write all data to a json file """
-    json_output_path = Path(json_output_path)
-    if not json_output_path.exists():
-        json_output_path.mkdir(parents=True)
-    if not model_name.endswith(".json"):
-        model_name += ".json"
-    with open(json_output_path / model_name, "w+") as outfile:
-        json.dump(viz_data, outfile)
-
-
-def convert_xml_to_json(path_to_xml_files):
-    """ convert a set of MultiCellDS XML files to a Simularium JSON file """
-    write_json_data(
-        convert_physicell_to_simularium(load_simulation_data(path_to_xml_files))
+def main():
+    parser = argparse.ArgumentParser(
+        description="Converts PhysiCell trajectories to "
+                    ".simularium JSON format for visualization"
     )
-    print("wrote Simularium JSON with {} agent types".format(last_id))
+    parser.add_argument(
+        "dir_path", help="the file path of the directory\
+         containing the trajectories to parse")
+    args = parser.parse_args()
+    
+    box_size = 1000.0
+    scale = 0.01
+
+    data = {
+        "box_size" : np.array([box_size, box_size, 1.0 / scale]),
+        "timestep" : 360.0,
+        "path_to_output_dir" : args.dir_path,
+        "types" : {
+            0 : { # tumor cell
+                0 : "tumor#G0G1",
+                1 : "tumor#S",
+                2 : "tumor#G2",
+                3 : "tumor#M",
+                4 : "tumor#apoptotic",
+                5 : "tumor#necrotic",
+            },
+            1 : { # motile tumor cell
+                0 : "motile tumor#G0G1",
+                1 : "motile tumor#S",
+                2 : "motile tumor#G2",
+                3 : "motile tumor#M",
+                4 : "motile tumor#apoptotic",
+                5 : "motile tumor#necrotic",
+            }
+        },
+        "scale_factor" : scale,
+    }
+    Converter(data, "physicell").write_JSON("physicell_trajectory")
+
+if __name__ == '__main__':
+    main()
