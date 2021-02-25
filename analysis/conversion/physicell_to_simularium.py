b5dcd5e41616e1133c9058292a345415592215ee
Blair Lyons <blair208@gmail.com>
2021-02-24 16:18:05 -0800

    updated physicell conversion to use the simulariumio.physicell module

--- analysis/conversion/physicell_to_simularium.py
+++ analysis/conversion/physicell_to_simularium.py
@@ -4,7 +4,10 @@
 import argparse
 
 import numpy as np
-from simulariumio import Converter
+from simulariumio.physicell import (
+    PhysicellConverter, 
+    PhysicellData,
+)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -12,18 +15,20 @@ def main():
                     ".simularium JSON format for visualization"
     )
     parser.add_argument(
-        "dir_path", help="the file path of the directory\
-         containing the trajectories to parse")
+        "dir_path", help="the file path of the PhysiCell\
+        output directory containing the trajectory to parse")
+    parser.add_argument(
+        "output_name", help="filename for resulting \
+        .simularium JSON file")
     args = parser.parse_args()
     
     box_size = 1000.0
-    scale = 0.01
 
-    data = {
-        "box_size" : np.array([box_size, box_size, 1.0 / scale]),
-        "timestep" : 360.0,
-        "path_to_output_dir" : args.dir_path,
-        "types" : {
+    data = PhysicellData(
+        box_size=np.array([box_size, box_size, 1.0]),
+        timestep=360.0,
+        path_to_output_dir=args.dir_path,
+        types={
             0 : {
                 "name" : "tumor cell",
                 4 : "A",
@@ -43,11 +48,11 @@ def main():
                 100 : "E",
                 101 : "F",
                 102 : "G",
-            }
+            },
         },
-        "scale_factor" : scale,
-    }
-    Converter(data, "physicell").write_JSON("physicell_trajectory")
+        scale_factor=0.01,
+    )
+    PhysicellConverter(data).write_JSON(args.output_name)
 
 if __name__ == '__main__':
     main()
