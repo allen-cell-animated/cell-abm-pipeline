983b20bd3a6cf61857a339c66505726c51d5a523
jessicasyu <jessicayu@u.northwestern.edu>
2022-04-18 14:24:14 -0400

    Added typing to initial conditions config

--- src/cell_abm_pipeline/initial_conditions/__config__.py
+++ src/cell_abm_pipeline/initial_conditions/__config__.py
@@ -1,20 +1,22 @@
+from typing import List
+
 # Pixel resolution for images (um/pixel) in x/y
-SCALE_MICRONS_XY = 0.108333
+SCALE_MICRONS_XY: float = 0.108333
 
 # Pixel resolution for images (um/pixel) in z
-SCALE_MICRONS_Z = 0.108333
+SCALE_MICRONS_Z: float = 0.108333
 
 # Threshold for number of samples touching edge of FOV to be considered edge
-EDGE_THRESHOLD = 1
+EDGE_THRESHOLD: int = 1
 
 # Maximum distance (in um) to nearest neighbor to be considered connected
-CONNECTED_THRESHOLD = 2.0
+CONNECTED_THRESHOLD: float = 2.0
 
 # List of Potts Hamiltonian terms for setup file
-POTTS_TERMS = ["volume", "surface", "adhesion", "height", "substrate", "persistence"]
+POTTS_TERMS: List[str] = ["volume", "surface", "adhesion", "height", "substrate", "persistence"]
 
 # Name of Quilt package for downloading images
-QUILT_PACKAGE = "aics/hipsc_single_cell_image_dataset"
+QUILT_PACKAGE: str = "aics/hipsc_single_cell_image_dataset"
 
 # Name of Quilt registry
-QUILT_REGISTRY = "s3://allencell"
+QUILT_REGISTRY: str = "s3://allencell"
