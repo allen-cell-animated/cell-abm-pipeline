58da0245f2fe74150924d3ad6c1a5e6fa2ea300d
jessicasyu <jessicayu@u.northwestern.edu>
2022-04-21 17:40:21 -0400

    Added find neighbors task

--- src/cell_abm_pipeline/colony_dynamics/__main__.py
+++ src/cell_abm_pipeline/colony_dynamics/__main__.py
@@ -2,15 +2,29 @@ import click
 
 
 class Context:
-    def __init__(self, working):
+    def __init__(self, name, keys, seeds, working):
+        self.name = name
+        self.keys = keys
+        self.seeds = seeds
         self.working = working
 
 
 @click.group(invoke_without_command=True)
+@click.option("-n", "--name", type=str, default="")
+@click.option("-k", "--keys", type=str, multiple=True, default=[])
+@click.option("-s", "--seeds", type=int, multiple=True, default=[])
 @click.option("-w", "--working", type=str, default=".")
 @click.pass_context
 def cli(ctx, **kwargs):
     ctx.obj = Context(**kwargs)
+
+
+@cli.command()
+@click.pass_obj
+def find_neighbors(obj, **kwargs):
+    from .find_neighbors import FindNeighbors
+
+    FindNeighbors(obj).run(**kwargs)
 
 
 if __name__ == "__main__":
---
+++ src/cell_abm_pipeline/colony_dynamics/find_neighbors.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from skimage import measure
+import scipy.ndimage as ndimage
+
+from cell_abm_pipeline.utilities.load import load_tar, load_tar_member
+from cell_abm_pipeline.utilities.save import save_dataframe
+from cell_abm_pipeline.utilities.keys import make_folder_key, make_file_key
+
+
+class FindNeighbors:
+    def __init__(self, context):
+        self.context = context
+        self.folders = {
+            "data.LOCATIONS": make_folder_key(context.name, "data", "LOCATIONS", False),
+            "analysis": make_folder_key(context.name, "analysis", "NEIGHBORS", True),
+        }
+        self.files = {
+            "data.LOCATIONS": make_file_key(context.name, ["LOCATIONS", "tar", "xz"], "%s", "%04d"),
+            "analysis": make_file_key(context.name, ["NEIGHBORS", "csv"], "%s", "%04d"),
+        }
+
+    def run(self):
+        for key in self.context.keys:
+            for seed in self.context.seeds:
+                self.find_neighbors(key, seed)
+
+    def find_neighbors(self, key, seed):
+        """
+        Finds neighbor connections for all cells.
+        """
+        loc_data_key = self.folders["data.LOCATIONS"] + self.files["data.LOCATIONS"] % (key, seed)
+        loc_data_tar = load_tar(self.context.working, loc_data_key)
+
+        all_neighbors = []
+        member_names = [member.name.split(".")[0] for member in loc_data_tar.getmembers()]
+
+        for member_name in tqdm(reversed(member_names)):
+            frame = member_name.split("_")[-1]
+            member = load_tar_member(loc_data_tar, f"{member_name}.LOCATIONS.json")
+
+            array = self.make_voxels_array(member)
+            neighbors = self.get_neighbors(array)
+
+            for region, voxel_id, neighbor_list in neighbors:
+                if len(neighbor_list) == 0:
+                    neighbor_list = [0]
+
+                for neighbor in neighbor_list:
+                    connection = {"REGION": region, "ID": voxel_id, "NEIGHBOR": neighbor}
+
+                    connection["KEY"] = key
+                    connection["SEED"] = seed
+                    connection["TICK"] = frame
+
+                    all_neighbors.append(connection)
+
+        neighbor_df = pd.DataFrame(all_neighbors)
+        analysis_key = self.folders["analysis"] + self.files["analysis"] % (key, seed)
+        save_dataframe(self.context.working, analysis_key, neighbor_df, index=False)
+
+    @staticmethod
+    def make_voxels_array(locations):
+        # Extract all voxel positions with id.
+        all_ids = []
+        all_xyz = []
+        for location in locations:
+            cell_id = location["id"]
+            xyz = [(x, y, z) for region in location["location"] for x, y, z in region["voxels"]]
+            all_xyz = all_xyz + xyz
+            all_ids = all_ids + [cell_id] * len(xyz)
+
+        # Create empty array.
+        mins = np.min(all_xyz, axis=0)
+        maxs = np.max(all_xyz, axis=0)
+        height, width, length = np.subtract(maxs, mins) + 3
+        array = np.zeros((height, width, length), dtype=np.uint16)
+
+        # Return if no voxels.
+        if len(all_ids) == 0:
+            return array
+
+        # Fill voxel array.
+        all_xyz_offset = [
+            (z - mins[0] + 1, y - mins[1] + 1, x - mins[2] + 1) for z, y, x in all_xyz
+        ]
+        array[tuple(np.transpose(all_xyz_offset))] = all_ids
+
+        return array
+
+    @staticmethod
+    def get_neighbors(array):
+        """Get list of neighbors for each unique ID in array."""
+        neighbors = []
+
+        # Create binary mask for array.
+        mask = np.zeros(array.shape, dtype="int")
+        mask[array != 0] = 1
+
+        # Label connected regions.
+        labels, regions = measure.label(mask, connectivity=2, return_num=True)
+
+        for region in range(1, regions + 1):
+            # Set all voxels outside of region of interest to zero.
+            array_region = array.copy()
+            array_region[labels != region] = 0
+
+            # Crop array to region.
+            xmin, xmax, ymin, ymax, zmin, zmax = FindNeighbors.get_bounding_box(array_region)
+            array_region_cropped = array_region[xmin:xmax, ymin:ymax, zmin:zmax]
+
+            # Iterate through each voxel id in region to get neighbors.
+            voxel_ids = np.unique(array_region_cropped)
+            voxel_ids = [i for i in voxel_ids if i != 0]
+
+            # Find neighbors for each voxel id.
+            for voxel_id in voxel_ids:
+                # Set all other voxels to zero.
+                voxel_region = array_region_cropped.copy()
+                voxel_region[voxel_region != voxel_id] = 0
+
+                # Crop array to voxel.
+                xmin, xmax, ymin, ymax, zmin, zmax = FindNeighbors.get_bounding_box(voxel_region)
+                voxel_region_cropped = array_region_cropped[xmin:xmax, ymin:ymax, zmin:zmax]
+
+                # Apply custom filter to get border locations.
+                border_mask = ndimage.generic_filter(
+                    voxel_region_cropped, (lambda v: voxel_id in v), size=3
+                )
+
+                # Find neighbors overlapping border.
+                neighbor_list = np.unique(voxel_region_cropped[border_mask == 1])
+                neighbor_list = [i for i in neighbor_list if i not in [0, voxel_id]]
+                neighbors.append([region, voxel_id, neighbor_list])
+
+        return neighbors
+
+    @staticmethod
+    def get_bounding_box(array):
+        """Finds bounding box around binary array."""
+        x, y, z = array.shape
+
+        xbounds = np.any(array, axis=(1, 2))
+        ybounds = np.any(array, axis=(0, 2))
+        zbounds = np.any(array, axis=(0, 1))
+
+        xmin, xmax = np.where(xbounds)[0][[0, -1]]
+        ymin, ymax = np.where(ybounds)[0][[0, -1]]
+        zmin, zmax = np.where(zbounds)[0][[0, -1]]
+
+        xmin = max(xmin - 1, 0)
+        xmax = min(xmax + 2, x)
+
+        ymin = max(ymin - 1, 0)
+        ymax = min(ymax + 2, y)
+
+        zmin = max(zmin - 1, 0)
+        zmax = min(zmax + 2, z)
+
+        return xmin, xmax, ymin, ymax, zmin, zmax
