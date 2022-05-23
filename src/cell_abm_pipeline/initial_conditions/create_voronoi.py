62f43d6565b59a28f46291e760535389a5df8e75
jessicasyu <jessicayu@u.northwestern.edu>
2022-05-23 16:01:14 -0400

    Updated voronoi task to handle edge cells

--- src/cell_abm_pipeline/initial_conditions/create_voronoi.py
+++ src/cell_abm_pipeline/initial_conditions/create_voronoi.py
@@ -1,4 +1,5 @@
-from scipy.ndimage import distance_transform_edt
+import numpy as np
+from scipy.ndimage import distance_transform_edt, binary_dilation
 
 from cell_abm_pipeline.utilities.load import load_image
 from cell_abm_pipeline.utilities.save import save_image
@@ -27,6 +28,15 @@ class CreateVoronoi:
         image = load_image(self.context.working, image_key)
 
         array = image.get_image_data("ZYX", T=0, C=channel)
+
+        # Create mask and expand using binary dilation to create an artifical
+        # border for the voronoi.
+        mask = np.zeros(array.shape, dtype="int")
+        mask[array != 0] = 1
+        mask = binary_dilation(mask, iterations=50)
+        mask_id = np.iinfo(array.dtype).max
+        array[mask == 0] = mask_id
+
         distances = distance_transform_edt(array == 0, return_distances=False, return_indices=True)
         distances = distances.astype("uint16", copy=False)
 
@@ -34,6 +44,10 @@ class CreateVoronoi:
         coordy = distances[1].flatten()
         coordx = distances[2].flatten()
         voronoi = array[coordz, coordy, coordx].reshape(array.shape)
+
+        # Remove masking ids.
+        voronoi[mask == 0] = 0
+        voronoi[voronoi == mask_id] = 0
 
         output_key = make_full_key(self.folders, self.files, "output", (key, channel))
         save_image(self.context.working, output_key, voronoi)
