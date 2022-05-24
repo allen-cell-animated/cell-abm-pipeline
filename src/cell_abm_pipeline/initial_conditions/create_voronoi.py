017c8164f2ce8b7c356f1beb27ef9d0d4150a63a
jessicasyu <jessicayu@u.northwestern.edu>
2022-05-23 21:07:28 -0400

    Added bounding box to voronoi task to reduce memory usage

--- src/cell_abm_pipeline/initial_conditions/create_voronoi.py
+++ src/cell_abm_pipeline/initial_conditions/create_voronoi.py
@@ -31,23 +31,53 @@ class CreateVoronoi:
 
         # Create mask and expand using binary dilation to create an artifical
         # border for the voronoi.
-        mask = np.zeros(array.shape, dtype="int")
+        mask = np.zeros(array.shape, dtype="uint8")
         mask[array != 0] = 1
-        mask = binary_dilation(mask, iterations=50)
+        mask = binary_dilation(mask, iterations=100)
         mask_id = np.iinfo(array.dtype).max
         array[mask == 0] = mask_id
 
-        distances = distance_transform_edt(array == 0, return_distances=False, return_indices=True)
+        xmin, xmax, ymin, ymax, zmin, zmax = self.get_bounded_array(mask)
+        subarray = array[zmin:zmax, ymin:ymax, xmin:xmax]
+
+        distances = distance_transform_edt(
+            subarray == 0, return_distances=False, return_indices=True
+        )
         distances = distances.astype("uint16", copy=False)
 
         coordz = distances[0].flatten()
         coordy = distances[1].flatten()
         coordx = distances[2].flatten()
-        voronoi = array[coordz, coordy, coordx].reshape(array.shape)
+        voronoi = subarray[coordz, coordy, coordx].reshape(subarray.shape)
 
         # Remove masking ids.
-        voronoi[mask == 0] = 0
-        voronoi[voronoi == mask_id] = 0
+        array[zmin:zmax, ymin:ymax, xmin:xmax] = voronoi
+        array[mask == 0] = 0
+        array[array == mask_id] = 0
 
         output_key = make_full_key(self.folders, self.files, "output", (key, channel))
-        save_image(self.context.working, output_key, voronoi)
+        save_image(self.context.working, output_key, array)
+
+    @staticmethod
+    def get_bounded_array(array):
+        """Finds bounding box around binary array."""
+        z, y, x = array.shape
+
+        zbounds = np.any(array, axis=(1, 2))
+        ybounds = np.any(array, axis=(0, 2))
+        xbounds = np.any(array, axis=(0, 1))
+
+        zmin, zmax = np.where(zbounds)[0][[0, -1]]
+        ymin, ymax = np.where(ybounds)[0][[0, -1]]
+        xmin, xmax = np.where(xbounds)[0][[0, -1]]
+
+        xmin = max(xmin - 1, 0)
+        xmax = min(xmax + 2, x)
+
+        ymin = max(ymin - 1, 0)
+        ymax = min(ymax + 2, y)
+
+        zmin = max(zmin - 1, 0)
+        zmax = min(zmax + 2, z)
+
+        return xmin, xmax, ymin, ymax, zmin, zmax
