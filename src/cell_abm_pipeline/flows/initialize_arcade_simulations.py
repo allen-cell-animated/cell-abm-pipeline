75fd9d123d9c67b435c73b096e28e380b2832b4f
jessicasyu <jesyu@uw.edu>
2022-10-28 16:15:25 -0400

    Update initialize arcade simulations flow

--- src/cell_abm_pipeline/flows/initialize_arcade_simulations.py
+++ src/cell_abm_pipeline/flows/initialize_arcade_simulations.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-from prefect import flow
+from prefect import flow, task
 
 from io_collection.keys import make_key, check_key_exists
 from io_collection.load import load_dataframe
 from io_collection.quilt import load_quilt_package, save_quilt_item
-from abm_initialization_collection.images import select_fov_images
+from abm_initialization_collection.image import select_fov_images
 
 
 @dataclass
@@ -31,11 +31,10 @@ class SeriesConfig:
 
     metadata_key: str
 
-
-@flow(name="initialize-simulations")
+@flow(name="initialize-arcade-simulations")
 def run_flow(context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig) -> None:
     package = load_quilt_package(parameters.quilt_package, parameters.quilt_registry)
-    key_exists = check_key_exists(context.metadata_location, series.metadata_key)
+    key_exists = check_key(context.metadata_location, series.metadata_key)
 
     if not key_exists:
         save_quilt_item(context.working_location, series.metadata_key, package, "metadata.csv")
@@ -50,9 +49,18 @@ def run_flow(context: ContextConfig, series: SeriesConfig, parameters: Parameter
 
     for fov in selected_fovs:
         fov_key = make_key(series.name, "images", f"{fov['key']}.ome.tiff")
-        key_exists = check_key_exists(context.working_location, fov_key)
+        key_exists = check_key(context.working_location, fov_key)
 
-        if key_exists:
-            continue
+        if not key_exists:
+            save_quilt_item.submit(context.working_location, fov_key, package, fov["item"])
 
-        save_quilt_item.submit(context.working_location, fov_key, package, fov["item"])
+        # command = [
+        #     "abmpipe",
+        #     "sample-image",
+        #     "::",
+        #     f"parameters.key={fov['key']}",
+        #     "parameters.channels=[0,1]",
+        #     f"context.working_location={context.working_location}",
+        #     f"series.name={series.name}",
+        # ]
+        # print(" ".join(command))
