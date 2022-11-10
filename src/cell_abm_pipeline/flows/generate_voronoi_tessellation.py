48ffdec1dd63ea747bd111091db9ecce6cca4081
jessicasyu <jesyu@uw.edu>
2022-11-10 12:31:54 -0500

    Add generate voronoi workflow

---
+++ src/cell_abm_pipeline/flows/generate_voronoi_tessellation.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from prefect import flow
+
+from io_collection.keys import make_key
+from io_collection.load import load_image
+from io_collection.save import save_image
+from abm_initialization_collection.image import create_voronoi_image
+
+
+@dataclass
+class ParametersConfig:
+    channel: int
+
+    iterations: int
+
+    target_height: int
+
+
+@dataclass
+class ContextConfig:
+    working_location: str
+
+
+@dataclass
+class SeriesConfig:
+    name: str
+
+    conditions: list[dict]
+
+
+@flow(name="generate-voronoi-tessellation")
+def run_flow(context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig) -> None:
+    for key in series.conditions:
+        image_key = make_key(series.name, "images", f"{series.name}_{key['key']}_T0000.tiff")
+        image = load_image(context.working_location, image_key, "ZYX")
+
+        voronoi = create_voronoi_image(
+            image, parameters.channel, parameters.iterations, parameters.target_height
+        )
+
+        voronoi_key = make_key(
+            series.name,
+            "images",
+            f"{series.name}_{key['key']}_T0000_C{parameters.channel:02}_voronoi.ome.tiff",
+        )
+        save_image(context.working_location, voronoi_key, voronoi)
