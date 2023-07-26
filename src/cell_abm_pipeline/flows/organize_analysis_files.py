d3fb20fc4c8347122583b15a2e359e9443d96cc0
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-07-26 14:54:44 -0400

    Consolidate organize flows into single flow

---
+++ src/cell_abm_pipeline/flows/organize_analysis_files.py
@@ -0,0 +1,167 @@
+"""
+Workflow for organizing analysis files.
+"""
+
+import re
+from dataclasses import dataclass
+from typing import Optional
+
+import pandas as pd
+from io_collection.keys import check_key, make_key, remove_key
+from io_collection.load import load_dataframe, load_tar
+from io_collection.save import save_dataframe, save_tar
+from prefect import flow
+
+
+@dataclass
+class ParametersConfig:
+    """Parameter configuration for organize analysis files flow."""
+
+    suffix: str
+
+    ticks: list[int]
+
+    region: Optional[str] = None
+
+
+@dataclass
+class ContextConfig:
+    """Context configuration for organize analysis files flow."""
+
+    working_location: str
+
+
+@dataclass
+class SeriesConfig:
+    """Series configuration for organize analysis files flow."""
+
+    name: str
+
+    seeds: list[int]
+
+    conditions: list[dict]
+
+
+@flow(name="organize-analysis-files")
+def run_flow(context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig) -> None:
+    """Main organize analysis files flow."""
+
+    # Iterate through conditions and seeds to merge contents of individual
+    # ticks into a single csv. If merged csv exists and the specified tick
+    # does not exist in the csv, the tick is appended. If the merged csv exists
+    # and specified tick exists in the csv, the tick is skipped.
+    run_flow_merge_files(context, series, parameters)
+
+    # Iterate through conditions and seeds to combine and compress individual
+    # ticks into a .tar.xz archive. If the archive exists and the specified
+    # tick is not in the archive, the tick is appended. If the archive exists
+    # and specified tick exists in the archive, the tick is skipped.
+    run_flow_compress_files(context, series, parameters)
+
+    # Iterate through conditions and seeds to remove individual ticks if the
+    # tick exists in the corresponding .tar.xz archive.
+    run_flow_remove_files(context, series, parameters)
+
+
+@flow(name="organize-analysis-files_merge-files")
+def run_flow_merge_files(
+    context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig
+) -> None:
+    suffix = parameters.suffix
+    analysis_key = make_key(series.name, "analysis", f"analysis.{suffix}")
+    region = f"_{parameters.region}" if parameters.region is not None else ""
+
+    for condition in series.conditions:
+        for seed in series.seeds:
+            series_key = f"{series.name}_{condition['key']}_{seed:04d}"
+            file_key = make_key(analysis_key, f"{series_key}{region}.{suffix}.csv")
+            file_key_exists = check_key(context.working_location, file_key)
+
+            existing_ticks = []
+            if file_key_exists:
+                existing_contents = load_dataframe(context.working_location, file_key)
+                existing_ticks = list(existing_contents["TICK"].unique())
+
+            contents = []
+
+            for tick in parameters.ticks:
+                if tick in existing_ticks:
+                    continue
+
+                tick_key = make_key(analysis_key, f"{series_key}_{tick:06d}{region}.{suffix}.csv")
+                contents.append(load_dataframe(context.working_location, tick_key))
+
+            if not contents:
+                continue
+
+            contents_dataframe = pd.concat(contents, ignore_index=True)
+
+            if file_key_exists:
+                contents_dataframe = pd.concat(
+                    [existing_contents, contents_dataframe], ignore_index=True
+                )
+
+            save_dataframe(context.working_location, file_key, contents_dataframe, index=False)
+
+
+@flow(name="organize-analysis-files_compress-files")
+def run_flow_compress_files(
+    context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig
+) -> None:
+    suffix = parameters.suffix
+    analysis_key = make_key(series.name, "analysis", f"analysis.{suffix}")
+    region = f"_{parameters.region}" if parameters.region is not None else ""
+
+    for condition in series.conditions:
+        for seed in series.seeds:
+            series_key = f"{series.name}_{condition['key']}_{seed:04d}"
+            file_key = make_key(analysis_key, f"{series_key}{region}.{suffix}.tar.xz")
+            file_key_exists = check_key(context.working_location, file_key)
+
+            existing_ticks = []
+            if file_key_exists:
+                existing_contents = load_tar(context.working_location, file_key)
+                existing_ticks = [
+                    int(re.findall(r"[0-9]{6}", member.name)[0])
+                    for member in existing_contents.getmembers()
+                ]
+
+            contents = []
+
+            for tick in parameters.ticks:
+                if tick in existing_ticks:
+                    continue
+
+                tick_key = make_key(analysis_key, f"{series_key}_{tick:06d}{region}.{suffix}.csv")
+                contents.append(tick_key)
+
+            if not contents:
+                continue
+
+            save_tar(context.working_location, file_key, contents)
+
+
+@flow(name="organize-analysis-files_remove-files")
+def run_flow_remove_files(
+    context: ContextConfig, series: SeriesConfig, parameters: ParametersConfig
+) -> None:
+    suffix = parameters.suffix
+    analysis_key = make_key(series.name, "analysis", f"analysis.{suffix}")
+    region = f"_{parameters.region}" if parameters.region is not None else ""
+
+    for condition in series.conditions:
+        for seed in series.seeds:
+            series_key = f"{series.name}_{condition['key']}_{seed:04d}"
+            file_key = make_key(analysis_key, f"{series_key}{region}.{suffix}.tar.xz")
+            file_key_exists = check_key(context.working_location, file_key)
+
+            if not file_key_exists:
+                continue
+
+            existing_contents = load_tar(context.working_location, file_key)
+
+            for member in existing_contents.getmembers():
+                tick_key = make_key(analysis_key, member.name)
+
+                if check_key(context.working_location, tick_key):
+                    remove_key(context.working_location, tick_key)
